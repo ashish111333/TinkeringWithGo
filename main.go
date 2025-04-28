b1783a55748e255b1b48468bee19ef6a3c0a2ea5
ashish111333 <ashishkumar0265aaa@gmail.com>
2025-04-29 00:04:51 +0530

    updated main.go
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- main.go
+++ main.go
@@ -5,5 +5,6 @@ func main() {
 	// fun.StrUsingBuilder is faster than fun.StrWithoutBuilder
 	// the builder one starts outperforming the later significantly
 	// after the number of strings become large (num of strings >10 appx)
+	// question: why does it perform better ? (Think)
 
 }
