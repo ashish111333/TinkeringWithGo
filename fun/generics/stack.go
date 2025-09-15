88fde3ca06ca22c373a741f85b2380d28f39b282
ashish111333 <ashishkumar0265aaa@gmail.com>
2025-09-15 21:11:54 +0530

    use slice instead of pointer to slice in stack
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- fun/generics/stack.go
+++ fun/generics/stack.go
@@ -7,52 +7,53 @@ import (
 
 // a simple generic stack implementation
 type Stack[T any] struct {
-	stackData *[]T
+	stackData []T
 }
 
 // returns an empty stack
 func NewStack[T any]() *Stack[T] {
 	s := []T{}
 	return &Stack[T]{
-		stackData: &s,
+		stackData: s,
 	}
 }
 
 // Append value at the end of stack , ss is the underlying stack slice
 func (s *Stack[T]) Push(val T) {
 	if s.stackData == nil {
-		s.stackData = &[]T{}
+		slc := []T{}
+		slc = append(slc, val)
+		s.stackData = slc
 	}
-	ss := *s.stackData
+	ss := s.stackData
 	ss = append(ss, val)
-	*s.stackData = ss
+	s.stackData = ss
 
 }
 
 // removes value from the top of the stack
 func (s *Stack[T]) Pop() (error, T) {
-	var pop_val T
-	stack_len := s.Stacklen()
-	if s.Stacklen() == 0 {
-		return cantPopEmptyStack, pop_val
+
+	var popval T
+	if s.stackData == nil {
+		return cantPopEmptyStack, popval
 	}
-	ss := *s.stackData
-	pop_val = ss[stack_len-1]
-	*s.stackData = ss[0 : stack_len-1]
-	return nil, pop_val
+	ss := s.stackData
+	popval = ss[len(ss)-1]
+	new_ss := ss[0 : len(ss)-1]
+	s.stackData = new_ss
+	return nil, popval
 
 }
 
 // returns the number of elements in the stack
 func (s *Stack[T]) Stacklen() int {
-
-	return len(*s.stackData)
+	return len(s.stackData)
 }
 
 // Prints the stack on screen
 func (s *Stack[T]) PrinTStack() {
-	ss := *s.stackData
-	fmt.Println(ss)
+	fmt.Println(s.stackData)
 }
 
 var (
