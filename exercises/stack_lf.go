a5295eca72c55f6fdfe6a7a74803ba31c6d7fe36
ashish111333 <ashishkumar0265aaa@gmail.com>
2026-01-01 02:25:15 +0530

    lock free stack
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

--- exercises/stack_lf.go
+++ exercises/stack_lf.go
@@ -1,3 +1,36 @@
 package exercises
 
-// TASK : Create lock free stack , so you will be using atomics
+import "sync/atomic"
+
+// lock free concurrent stack , so you will be using atomics
+
+// each node stores stack data and a down_ptr pointing to node below
+// stack (this one ) grows in bottom to up manner
+type node[T interface{}] struct {
+	data     T
+	down_ptr *node[T]
+}
+
+type Stack[T interface{}] struct {
+	head atomic.Pointer[*node[T]]
+}
+
+func (s *Stack[T]) Push(x T) {
+	newNode := &node[T]{
+		data: x,
+	}
+	if !s.head.CompareAndSwap(nil, &newNode) {
+		currHead := s.head.Load()
+		newNode.down_ptr = *currHead
+		s.head.Store(&newNode)
+	}
+
+}
+
+// change head to second last element from top
+func (s *Stack[T]) Pop() {
+	var currHead *node[T] = *s.head.Load()
+	preNode := currHead.down_ptr
+	s.head.Store(&preNode)
+
+}
